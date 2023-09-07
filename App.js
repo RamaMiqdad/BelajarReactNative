293d5c36aefb0abdf672f90d64d697e92b383533
RamaMiqdad <ramamiqdad76@gmail.com>
2023-09-07 18:31:50 +0700

    learn2

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StyleSheet, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+import StartGamesScreen from "./screens/StartGameScreen";
+
+export default function App() {
+  return (
+    <LinearGradient colors={["#ddb52f", "#4e0329"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGamesScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
